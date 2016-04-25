ca439e70f2039f24c8a346c4d081ff3fb1039cf5
XuYun <yzb808@126.com>
2016-04-25 10:16:17 +0800

    first premain commit

---
+++ src/main/java/com/yzb808/main/Main.java
@@ -0,0 +1,13 @@
+package com.yzb808.main;
+
+public class Main {
+
+	/**
+	 * 执行main方法前，启动配置中使用-javaagent:E:\eclipse\workSpace\premain\target\premain-0.0.1.jar
+	 * 指定包含premain方法的jar包绝对路径
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello main!");
+	}
+}
