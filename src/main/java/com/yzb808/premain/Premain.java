530906f5122abfea78341ccc80781a474311e9cf
XuYun <yzb808@126.com>
2016-05-10 09:42:17 +0800

    premain fix

--- src/main/java/com/yzb808/premain/Premain.java
+++ src/main/java/com/yzb808/premain/Premain.java
@@ -10,13 +10,14 @@ public class Premain {
 	
 	/**
 	 * 1. 如果改变premain方法中的内容，要重新打jar包才能生效
-	 * 2. jar包中的META-INF/MAINIFEST.MF必须包含 Premain-Class这个属性
+	 * 2. jar包中的META-INF/MAINIFEST.MF必须包含 Premain-Class这个属性，指向premain这个类
 	 * 3. 一个java程序中-javaagent参数可以添加任意多个java agent，并按照添加顺序执行
 	 * 	例：java -javaagent:MyAgent1.jar -javaagent:MyAgent2.jar -jar MyProgram.jar
 	 * 4. 每一个java agent 都可以接收一个字符串类型的参数
 	 * 	例：java -javaagent:MyAgent2.jar=thisIsAgentArgs -jar MyProgram.jar
 	 */
 	public static void premain(String agentArgs, Instrumentation inst) {
+		System.out.println("agentArgs:" + agentArgs);
 		System.out.println("Hello premain! Premain's classLoader:" + Premain.class.getClassLoader());
 	}
 }
